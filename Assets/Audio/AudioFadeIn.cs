f25884db20f5a88e29e1689f9c66dea837e6c119
RarCeth <rarceth@gmail.com>
2016-08-19 12:59:28 +0800

    Update 1.6.0

---
+++ Assets/Audio/AudioFadeIn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFadeIn : MonoBehaviour {
+	public int approxSecondsToFade = 10;
+	private AudioSource audio;
+
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void FixedUpdate ()
+	{
+		if (audio.volume < 1)
+		{
+			audio.volume = audio.volume + (Time.deltaTime / (approxSecondsToFade + 1));
+		}
+		else
+		{
+			Destroy (this);
+		}
+	}
+}
